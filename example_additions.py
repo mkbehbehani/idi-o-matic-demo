c6fa0825112aec11968962495566bb6129d07c38
Mashallah Behbehani <mkbehbehani@gmail.com>
2019-03-03 11:12:49 -0600

    check if process is still alive

--- example_additions.py
+++ example_additions.py
@@ -3,3 +3,11 @@ def get_host(self):
 
 def start_app(self):
     self.flask_app.run(debug=self.debug)
+
+def is_running(pid):
+    try:
+        os.kill(pid, 0)
+    except OSError as err:
+        if err.errno == errno.ESRCH:
+            return False
+    return True
